62fd243b67bc47ea5b96c3165f8edfe5a6242065
bakerhassan <61099003+bakerhassan@users.noreply.github.com>
2024-03-27 01:54:08 -0400

    d

--- src/denoising.py
+++ src/denoising.py
@@ -160,7 +160,7 @@ if __name__ == "__main__":
 
     parser.add_argument("-patch2loc", "--patch2loc", type=bool, default=False,
                         help="use patch2loc")
-    parser.add_argument("-split_num", "--split_number", type=int, default=1,
+    parser.add_argument("-split_num", "--split_num", type=int, default=1,
                         help="which split number to use (if any)")
     args = parser.parse_args()
 
